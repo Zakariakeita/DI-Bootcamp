f17e190f74009f9887cca20c4e8ae878ce703bcb
Zakariakeita <zakariacheickoumarkeita@gmail.com>
2022-09-05 14:13:52 +0000

    first commit

--- Week2/Day5/ExerciseXPGold /script.js
+++ Week2/Day5/ExerciseXPGold /script.js
@@ -1,18 +1,69 @@
+let nombre;
 function number(num){
    
-   document.getElementById("myInput").innerHTML=Number(num);
+    nombre=document.getElementById("myInput").textContent;
+    document.getElementById("myInput").innerHTML= (nombre.toString().concat(Number(num)));
+  
 }
 
 
 function operator(operator){
+    nombre=document.getElementById("myInput").textContent;
     switch(operator)
     {
-        case '*':
-        
+        case "*": if(nombre.toString().charAt(nombre.length-1) =="*" || nombre.toString().charAt(nombre.length-1) =="-" || nombre.toString().charAt(nombre.length-1) =="+" ||nombre.toString().charAt(nombre.length-1) =="/" )
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().substring(0,nombre.length-1) + "*");break;
+
+                    }
+                    else
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().concat("*"));break;
+
+                    }
+        case "+":if(nombre.toString().charAt(nombre.length-1) =="*" || nombre.toString().charAt(nombre.length-1) =="-" || nombre.toString().charAt(nombre.length-1) =="+" ||nombre.toString().charAt(nombre.length-1) =="/" )
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().substring(0,nombre.length-1) + "+");break;
+
+                    }
+                    else
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().concat("+"));break;
+
+                    }
+        case "-":if(nombre.toString().charAt(nombre.length-1) =="*" || nombre.toString().charAt(nombre.length-1) =="-" || nombre.toString().charAt(nombre.length-1) =="+" ||nombre.toString().charAt(nombre.length-1) =="/" )
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().substring(0,nombre.length-1) + "-");break;
+
+                    }
+                    else
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().concat("-"));break;
+
+                    }
+        case "/":if(nombre.toString().charAt(nombre.length-1) =="*" || nombre.toString().charAt(nombre.length-1) =="-" || nombre.toString().charAt(nombre.length-1) =="+" ||nombre.toString().charAt(nombre.length-1) =="/" )
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().substring(0,nombre.length-1) + "/");break;
+
+                    }
+                    else
+                    {
+                        document.getElementById("myInput").innerHTML=(nombre.toString().concat("/"));break;
+
+                    }
     }
-    document.getElementById("myInput").innerHTML=Number(num);
-   
 }
+
 function equal(){
-   
+    document.getElementById("myInput").innerHTML=eval(document.getElementById("myInput").textContent.toString());
+}
+
+function effacerOne()
+{
+    nombre=document.getElementById("myInput").textContent.toString();
+    document.getElementById("myInput").innerHTML=(nombre.substring(0,nombre.length-1)); 
+}
+function effacerAll()
+{
+    document.getElementById("myInput").innerHTML=" ";    
 }
--- Week2/Day5/ExerciseXPNinja/script.js
+++ Week2/Day5/ExerciseXPNinja/script.js
@@ -1,109 +1,45 @@
 //Exercise 1
-    console.log("Exercise 1 : ");
-    let num= Math.floor(Math.random() * (100-1) +1);
-    for(let i=0; i<=num;i++)
+    console.log("Hangman");
+    let mot;
+    let lettre;
+    do
     {
-       if(i%2==0){
-        console.log(i);
-        }
-    }
-   
+       mot=prompt("Enter a word");
+    }while(mot.length<8);
+    //let mot2=mot.substring(0,)
+    //console.log("".padStart(mot.length,"*"));
     
-//****************************************************
-
-//Exercise 2
-    console.log("Exercise 2 : ");
-    function capitalize(mot) {
-        mot=mot.toLowerCase();
-        let tab=[];
-        for (let i = 0; i < mot.length; i++) {
-            if (i%2==0) {
-                mot=mot.replace(mot[i],mot[i].toUpperCase());
-            }
-        }
-        tab.push(mot);
-        mot=mot.toLowerCase();
-        for (let i = 0; i < mot.length; i++) {
-            if (!(i%2==0)) {
-                mot=mot.replace(mot[i],mot[i].toUpperCase());
-            }
-        }
-        tab.push(mot);
-
-        console.log(tab);
-    }
-    capitalize("abcdef");
-
-//****************************************************
-
-//Exercise 3
-    console.log("Exercise 3 : ");
-    function palindrome(mot) {
-
-    for (let i = 0; i < (mot.length/2); i++) {
-
-        if (mot[i] != mot[(mot.length - 1) - i]) {
-            return (mot +" is not a palindrome");
-        }
-    }
-    return (mot +" is  a palindrome");
-    }
-
-    console.log(palindrome("ahd"));
-
-//****************************************************
-
-//Exercise 4
-    console.log("Exercise 4 : ");
-    function biggestNumberInArray(arrayNumber)
+   /* do
     {
-        let j=0;
-        while(isNaN(Number(arrayNumber[j])) && j<arrayNumber.length-1 )
-        {  
-            j++;
+       lettre=prompt("Enter a letter");
+       if(lettre in mot)
+       {
+            console.log("".padStart(mot.length,"*"));
+             mot=prompt("Enter a word");
         }
-        if(isNaN(Number(arrayNumber[j])) || arrayNumber.length==0)
-        {  
-            return 0;
-        }
-        else
-        {
-            let temp=arrayNumber[j];
-            
-           for (let i = 0; i < arrayNumber.length; i++) 
-            {
-                if(typeof(arrayNumber[i]) == 'string')
-                {
-                    continue;
-                }
-    
-                if(temp<arrayNumber[i] )
-                {
-                    temp=arrayNumber[i];
-                }
-                
-            }
-           return temp;
-        }
-        
-    }
-    console.log(biggestNumberInArray([-1,0,3,100, 99, 2, 99]));
-    console.log(biggestNumberInArray(['a', 3, 4, 2]));
-    console.log(biggestNumberInArray([]));
 
-//****************************************************
+    }while(mot.length<8)*/
 
-//Exercise 5
-    console.log("Exercise 5 : ");
-    function uniqueElement(tab) {
-        let tab2 = [];
-        for(let i = 0; i < tab.length; i++) {
-            if(!tab2.includes(tab[i])) {
-                tab2.push(tab[i]);
-            }
-        }
-        return tab2; 
+    let code=[];
+    for(let i=0;i<mot.length;i++)
+    {
+        code[i]="*"
     }
-        console.log(uniqueElement([1,2,3,3,3,3,4,5]));
-        console.log(uniqueElement([1,2,3,3,3,3,4,5]));
-    
+    console.log(mot);
+
+    let taille=mot.length
+
+    while(taille>0)
+    {
+       let letter=prompt("Enter a letter");
+       
+        if(letter.length !=1)
+       {
+        console.log("you have alreay chosen this");
+       
+        }
+        else if(mot.includes(letter))
+       {
+            break;
+        }
+    }
