ab29e868eb88f3232b9ddee51a0c3124468da1f8
Zakariakeita <zakariacheickoumarkeita@gmail.com>
2022-09-22 09:59:49 +0000

    first commit

--- Week4/Day1/DaiyChallenge/Index.py
+++ Week4/Day1/DaiyChallenge/Index.py
@@ -7,10 +7,10 @@ if len(thestring) > 10 :
 elif len(thestring) < 10:
     print("string not long enough")
 else :
-	print(" first character : "+thestring[0]+"\n last character :"+thestring[-1])
-	print('Then, you have to construct the string character by character')
-	for i in thestring : 
-	    construct += i
-	    print(construct)
-	n=''.join(random.sample(thestring,len(thestring)))
-	print("new string : "+n)
+    print(" first character : "+thestring[0]+"\n last character :"+thestring[-1])
+    print('Then, you have to construct the string character by character')
+    for i in thestring : 
+        construct += i
+        print(construct)
+    newString="".join(random.sample(thestring,len(thestring)))
+    print("new string : " + newString)
